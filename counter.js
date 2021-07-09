01810025431beb3ebfc1b1e8bb4d7d2a4086b874
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-07-09 14:27:04 +0600

    Counter Partially fixed

--- counter.js
+++ counter.js
@@ -1,10 +1,9 @@
-let now = 0;
+window.addEventListener('scroll', function () {
+    var element = document.querySelector('#counter-view');
+    var position = element.getBoundingClientRect();
 
-window.addEventListener('scroll', () => {
-    const scrolled = window.scrollY;
-    if (scrolled >= 400 && scrolled <= 1200) now = 1;
-    else now = 0;
-    if (now) {
+    // checking whether fully visible
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
         var counter1 = document.querySelector(".count1");
         var counter2 = document.querySelector(".count2");
         var counter3 = document.querySelector(".count3");
@@ -32,103 +31,12 @@ window.addEventListener('scroll', () => {
             }
         }, 50)
     }
-})
 
-// setTimeout(() => {
-//     var counter1 = document.querySelector(".count1");
-//     var counter2 = document.querySelector(".count2");
-//     var counter3 = document.querySelector(".count3");
-//     let coun1 = 0;
-//     let coun2 = 0;
-//     let coun3 = 0;
+    // checking for partial visibility
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        console.log('Element is partially visible in screen');
+    }
+});
 
-//     setInterval(() => {
-//         if (coun1 < 150) {
-//             coun1 += 7.5;
-//             counter1.innerText = Math.round(coun1);
-//         }
-//     }, 50)
-
-//     setInterval(() => {
-//         if (coun2 < 980) {
-//             coun2 += 49;
-//             counter2.innerText = "" + Math.round(coun2) + 'K';
-//         }
-//     }, 50)
-
-//     setInterval(() => {
-//         if (coun3 < 2500) {
-//             coun3 += 125;
-//             counter3.innerText = Math.round(coun3);
-//         }
-//     }, 50)
-// }, 2000)
-
-// let now = 0;
-// let coun1 = 0;
-// let coun2 = 0;
-// let coun3 = 0;
-// window.addEventListener('scroll', () => {
-//     const scrolled = window.scrollY;
-//     if (scrolled >= 400 && scrolled <= 1200 && coun1 == 0 && coun2 == 0 && coun3 == 0) now = 1;
-//     else coun3 = 0, coun2 = 0, coun1 = 0, now = 0;
-
-//     if (now) {
-//         var counter1 = document.querySelector(".count1");
-//         var counter2 = document.querySelector(".count2");
-//         var counter3 = document.querySelector(".count3");
-//         setInterval(() => {
-//             if (coun1 < 150) {
-//                 coun1 += 7.5;
-//                 counter1.innerText = Math.round(coun1);
-//             }
-//         }, 50)
-
-//         setInterval(() => {
-//             if (coun2 < 980) {
-//                 coun2 += 49;
-//                 counter2.innerText = "" + Math.round(coun2) + 'K';
-//             }
-//         }, 50)
-
-//         setInterval(() => {
-//             if (coun3 < 2500) {
-//                 coun3 += 125;
-//                 counter3.innerText = Math.round(coun3);
-//             }
-//         }, 50)
-
-//     }
-// })
-
-// setTimeout(() => {
-//     var counter1 = document.querySelector(".count1");
-//     var counter2 = document.querySelector(".count2");
-//     var counter3 = document.querySelector(".count3");
-//     let coun1 = 0;
-//     let coun2 = 0;
-//     let coun3 = 0;
-
-//     setInterval(() => {
-//         if (coun1 < 150) {
-//             coun1 += 7.5;
-//             counter1.innerText = Math.round(coun1);
-//         }
-//     }, 50)
-
-//     setInterval(() => {
-//         if (coun2 < 980) {
-//             coun2 += 49;
-//             counter2.innerText = "" + Math.round(coun2) + 'K';
-//         }
-//     }, 50)
-
-//     setInterval(() => {
-//         if (coun3 < 2500) {
-//             coun3 += 125;
-//             counter3.innerText = Math.round(coun3);
-//         }
-//     }, 50)
-// }, 2000)
 
 
